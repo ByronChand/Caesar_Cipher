c00c8bc60f3794f6e55400b357f894dda2cfa184
Byron <byron@Byrons-MacBook-Pro.local>
2025-11-14 15:46:14 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Caesar Cipher Project
+
+This project implements a simple **Caesar Cipher**, which is a substitution cipher that shifts letters of the alphabet by a chosen amount.  
+It supports:
+
+- Encrypting text  
+- Decrypting text  
+- Validating shift values  
+
+---
+
+## Features
+
+✔ Encrypt messages  
+✔ Decrypt messages  
+✔ Shift must be between 1 and 25  
+✔ Preserves uppercase and lowercase  
+✔ Ignores non-letter characters  
+✔ Includes helper functions: `encrypt()` and `decrypt()`
+
+## How to Run
+
+### 1. Create and activate a virtual environment
+
+**macOS / Linux**
+python3 -m venv .venv
+source .venv/bin/activate
+
+**Windows**
+python -m venv .venv
+..venv\Scripts\Activate.ps1
+
+---
+
+### 2. Install dependencies
+
+pip install -r requirements.txt
+
+---
+
+### 3. Run the program
+
+python caesar.py
+
+---
+
+## Example Usage
+
+### Encrypt:
+encrypt("hello", 3)
+
+→ "khoor"
+
+### Decrypt:
+decrypt("khoor", 3)
+
+→ "hello"
+
+---
+
+## Project Structure
+
+myproject/
+├── caesar.py
+├── README.md
+├── requirements.txt
+├── .gitignore
+├── tests/
+│ └── test_caesar.py
+└── .venv/
---
+++ caesar_cipher.py
@@ -0,0 +1,25 @@
+def caesar(text, shift, encrypt=True):   
+    #if shift is not an integer, will get 'Shift Needs to be an Integer'
+    if not isinstance(shift, int):
+        return 'Shift Needs to be an Integer'
+    #shift needs to be between 1 and 25
+    if shift < 1 or shift > 25:
+        return 'Shift Needs to be an Integer Between 1 and 25'
+    #if encrypt is false, the shift will reverse and go in the reverse direction to decrypt an encrypted message
+    if not encrypt:
+        shift = - shift
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift] #shifts alphabet to a certain shift length by starting the alphabet at the shift number and adding the remaining alphabet ending at the shift integer
+    # Makes it so that you can replace the letters with the shifted alphabet using translate
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    return text.translate(translation_table)
+# use encrypt to encrypt and decrypt to decrypt and decrypt is done by turning encrypt to false which reverses the shif in the previous function
+def encrypt(text, shift):
+    return caesar(text, shift)
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+text = 'Hello'
+shift = 5
+encrypted_text = encrypt(text, shift)
+decrypted_text = decrypt(text, shift)
+print(encrypted_text)
